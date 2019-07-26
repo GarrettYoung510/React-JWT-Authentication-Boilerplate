07a1e75c4bbe76eb2b9ed0d75637161bf2757498
Garrett Young <garrettyoung510@gmail.com>
2019-07-26 14:09:49 -0700

    configure sign in and middlewares

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,15 @@
 // give us access to all the data that models exports
 const db = require("./../models");
+const jwt = require("jwt-simple");
+const config = require("./../config.js");
+
+const tokenForUser = user => {
+  const timestamp = new Date().getTime();
+  // sub is subject
+  // iat is issued at time
+  // It is going to encode the whole 1st object and add our secret to it thus JSON web token
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+};
 
 module.exports = {
   // signup method async and await & try catch
@@ -13,25 +23,26 @@ module.exports = {
     }
     // look for an existing user
     try {
-        // await goes in try block
-        const existingUser = await db.User.findOne({email});
-        if(existingUser){
-            // 422 error
-            return res.status(422).json({ error: 'Email is in use'});
-        }
-        // if user does not exist
-        // shorthand for email = email and password = password variables
-        const user = new db.User({ email, password });
-        // save the user
-        await user.save();
-        res.json(user);
+      // await goes in try block
+      const existingUser = await db.User.findOne({ email });
+      if (existingUser) {
+        // 422 error
+        return res.status(422).json({ error: "Email is in use" });
+      }
+      // if user does not exist
+      // shorthand for email = email and password = password variables
+      const user = new db.User({ email, password });
+      // save the user
+      await user.save();
+      //   sending user through token for user to forever authenticate our user
+      res.json({ token: tokenForUser(user) });
     } catch (e) {
-        // send 404 status and what error they got back
-        res.status(404).json({e});
+      // send 404 status and what error they got back
+      res.status(404).json({ e });
     }
   },
 
   signIn: (req, res) => {
-      res.send('im hit');
+    res.send("im hit");
   }
 };
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,17 @@
 const router = require("express").Router();
-const authRoutes = require('./authRoutes');
+const authRoutes = require("./authRoutes");
 
+// require passport
+const passportService = require('./../../services/passport');
+
+const authMiddleware = require("./../../middlewares/authMiddlewares.js");
 // /.api prepended to these routes
+
+router.route("/test").get(authMiddleware.requireAuth, (req, res) => {
+  res.send(req.user);
+});
+// require all these to protect our routes
+
 router.use("/auth", authRoutes);
 
 module.exports = router;
